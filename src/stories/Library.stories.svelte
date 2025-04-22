6faeab709cd44cecd8d9e3c4c2b56bf31d1ed85e
Miguel Angel Alarcos Torrecillas <miguelangel.alarcos@proton.me>
2025-04-22 13:30:17 +0200

    fixed story to new data received as initialContents

--- src/stories/Library.stories.svelte
+++ src/stories/Library.stories.svelte
@@ -15,16 +15,11 @@
       }
     });
 
-    const initialContents = [
-      { directory: 'the rolling stones' },
-      { file: 'the rolling stones - satisfaction.mp3', duration: 200 },
-      { directory: 'the beatles' },
-      { file: 'the beatles - hey jude.mp3', duration: 300 },
-      { file: 'the beatles - let it be.mp3', duration: 240 },
-      { directory: 'pink floyd' },
-      { file: 'pink floyd - time.mp3', duration: 400 },
-      { file: 'pink floyd - comfortably numb.mp3', duration: 500 }
-    ];
+    const initialContents = {
+      directories: [ 'the rolling stones', 'the beatles', 'pink floyd' ],
+      files: [ 'the rolling stones - satisfaction.mp3', 'the beatles - hey jude.mp3', 'the beatles - let it be.mp3', 'pink floyd - time.mp3', 'pink floyd - comfortably numb.mp3' ],
+    };
+
 </script>
   
 <Story name="Biblioteca" args={{ currentFolder: "Depeche mode", initialContents }} /> 

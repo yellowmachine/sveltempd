0ffdf0d2fea9cdf7c0524dffe63e7def9fc907ab
Miguel Angel Alarcos Torrecillas <miguelangel.alarcos@proton.me>
2025-04-26 18:40:32 +0200

    styled Setup component

--- src/lib/components/Setup.svelte
+++ src/lib/components/Setup.svelte
@@ -15,26 +15,27 @@
     }
   </script>
   
-  <h2 class="text-2xl font-bold mb-4">Configuración del Servidor</h2>
-  <form class="max-w-sm mx-auto mt-8 p-6 bg-white rounded shadow" onsubmit={handleSubmit}>
+  <form class="max-w-sm mx-auto mt-8 p-6 bg-white rounded border border-orange-300" onsubmit={handleSubmit}>
+    <h2 class="text-2xl font-bold mb-4 text-orange-600">Datos de ssh al servidor mpd</h2>
     <div class="mb-4">
-      <label class="block text-gray-700 font-bold mb-2" for="ip">
+      <label class="block text-gray-700 font-bold mb-2 text-orange-600" for="ip">
         Ip
       </label>
       <input
-        class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-orange-500"
         id="ip"
         type="text"
         bind:value={ip}
         required
+        placeholder="localhost"
       />
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 font-bold mb-2" for="username">
+      <label class="block text-gray-700 font-bold mb-2 text-orange-600" for="username">
         Usuario
       </label>
       <input
-        class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-orange-500"
         id="username"
         type="text"
         bind:value={username}
@@ -43,11 +44,11 @@
       />
     </div>
     <div class="mb-6">
-      <label class="block text-gray-700 font-bold mb-2" for="password">
+      <label class="block text-gray-700 font-bold mb-2 text-orange-600" for="password">
         Contraseña
       </label>
       <input
-        class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-oreange-500"
         id="password"
         type="password"
         bind:value={password}
@@ -57,7 +58,7 @@
     </div>
     <button
       type="submit"
-      class="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition"
+      class="w-full bg-orange-600 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded transition"
     >
       Guardar configuración
     </button>
---
+++ src/stories/Setup.stories.svelte
@@ -0,0 +1,14 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import Setup from '$lib/components/Setup.svelte';
+
+    const { Story } = defineMeta({
+      title: 'Admin/Setup',
+      component: Setup,
+      tags: ['autodocs'],
+      
+    });
+
+</script>
+  
+<Story name="Primary" />
